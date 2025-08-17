03eb17c13e87805deaf305342fb0322926f53f27
mgismissing <gabrycraft2010@gmail.com>
2025-08-17 17:50:36 +0200

    Change accent color for docs

--- docs/conf.py
+++ docs/conf.py
@@ -32,5 +32,13 @@ html_favicon = '_favicon.png'
 html_theme_options = {
     "light_logo": "logo_light.png",
     "dark_logo": "logo_dark.png",
-    "sidebar_hide_name": True
+    "sidebar_hide_name": True,
+    "light_css_variables": {
+        "color-brand-primary": "linear-gradient(180deg,rgba(77, 111, 102, 1) 0%, rgba(35, 88, 83, 1) 50%, rgba(18, 61, 66, 1) 100%)",
+        "color-brand-content": "linear-gradient(180deg,rgba(77, 111, 102, 1) 0%, rgba(35, 88, 83, 1) 50%, rgba(18, 61, 66, 1) 100%)"
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "linear-gradient(180deg,rgba(107, 191, 132, 1) 0%, rgba(91, 189, 153, 1) 100%);",
+        "color-brand-content": "linear-gradient(180deg,rgba(107, 191, 132, 1) 0%, rgba(91, 189, 153, 1) 100%);"
+    }
 }
